87d0dee528aabfce52a2f4dfc8e91b2330b4a642
isao92 <01agustin92@gmail.com>
2019-05-19 21:59:31 -0700

    dist folder git ignore fix

---
+++ vue-dental-new/dist/precache-manifest.51ca50a8a8f5b2e94d196e481786a289.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd6add3ee95157e6e8a4c57659ccd8b7",
+    "url": "/img/teeth-one.bd6add3e.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cdc5a66398f94a94caa887cbba59792e",
+    "url": "/img/teeth-eight.cdc5a663.jpg"
+  },
+  {
+    "revision": "b7668afd8aca6ed3b34a",
+    "url": "/js/chunk-vendors.2c4b2e57.js"
+  },
+  {
+    "revision": "839740b11a72aa0d25a1973983252b1d",
+    "url": "/img/teeth-five.839740b1.jpg"
+  },
+  {
+    "revision": "513375dca56e4221103c617870acad08",
+    "url": "/img/teeth-six.513375dc.jpg"
+  },
+  {
+    "revision": "1134670664dc009febaa12c3cba90486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7119b0d66f1f1acab061",
+    "url": "/js/app.99a9f05c.js"
+  },
+  {
+    "revision": "21ab16a4b37908bf1b2e0f0698ff570b",
+    "url": "/img/teeth-three.21ab16a4.jpg"
+  },
+  {
+    "revision": "d6f47f6b91ca6a49e4a021c5c6f087a7",
+    "url": "/img/teeth-two.d6f47f6b.jpg"
+  },
+  {
+    "revision": "e267acc5e79195b69eaf446ae1600dba",
+    "url": "/img/tooth-diff.e267acc5.jpg"
+  },
+  {
+    "revision": "6fd752bcde3bf101f2205923836e8c65",
+    "url": "/img/teeth-seven.6fd752bc.jpg"
+  },
+  {
+    "revision": "87754ed115ec0ad90c20d8453c155e18",
+    "url": "/img/teeth-nine.87754ed1.jpg"
+  },
+  {
+    "revision": "4f5942c0cfc862ca26f7684101bb26d5",
+    "url": "/img/teeth-four.4f5942c0.jpg"
+  },
+  {
+    "revision": "7119b0d66f1f1acab061",
+    "url": "/css/app.c63945f1.css"
+  }
+];
